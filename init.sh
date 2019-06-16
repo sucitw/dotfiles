7926f4ccb34a12b822f2651e6fe391cb34480145
shuhsi <sucitw@gmail.com>
2019-06-16 15:47:47 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -11,4 +11,6 @@ git clone --quiet https://github.com/sucitw/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 ln -fs `pwd`/vimrcs/basic.vim ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
+ln -fs `pwd`/bash_profile  ~/.bash_profile
+echo "Update bash_profile"
 
