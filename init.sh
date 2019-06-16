09a60f4cb3741e1d32c999bf31c52f458414870a
shuhsi <sucitw@gmail.com>
2019-06-16 19:31:33 +0800

    add tmux.conf

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 ## setup dotfiles
 
 # load dotfiles
-git clone --quiet https://github.com/sucitw/dotfiles.git ~/.dotfiles
+git clone --quiet https://github.com/sucitw/dotfiles.git ~/dotfiles
 
 #load Tmux plugins
 #git clone --quiet https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -11,6 +11,10 @@ git clone --quiet https://github.com/sucitw/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 ln -fs `pwd`/vimrcs/basic.vim ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
+
 ln -fs `pwd`/bash_profile  ~/.bash_profile
+ln -fs `pwd`/bash_aliases  ~/.bash_aliases
 echo "Update bash_profile"
 
+ln -fs `pwd`/tmux.conf  ~/.tmux.conf
+echo "Update tmux"
