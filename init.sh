af30292af1d7016ee8e2a6a407d13d5d5f4b2af0
sucitw <sucitw@gmail.com>
2020-05-23 10:24:43 +0000

    modify: update init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,56 @@
 #! bin/bash
 ## setup dotfiles
 
-# load dotfiles
-git clone --quiet https://github.com/sucitw/dotfiles.git ~/.dotfiles
+#==========
+# Variables
+#==========
+dotfiles_dir=~/dotfiles 
+log_fil=~/install-progress_log.txt
 
-#load Tmux plugins
-#git clone --quiet https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#=========
+# Deltet existing dot files and folders
+#=========
+rm -rf ~/.vim > /dev/null 2>&1
+rm -rf ~/.vimrc > /dev/null 2>&1
+rm -rf ~/.bashrc > /dev/null 2>&1
+rm -rf ~/.bash_profile > /dev/null 2>&1
+rm -rf ~/.bash_aliases > /dev/null 2>&1
+rm -rf ~/.config > /dev/null 2>&1
 
-# symbolic link dotfiles
-cd ~/.dotfiles
-ln -fs `pwd`/vimrcs/basic.vim ~/.vimrc
+
+# Git clone dotfiles
+git clone https://github.com/sucitw/dotfiles.git $dotfiles_dir
+
+#==============
+# Create symlinks in the home folder
+#==============
+ln -sf $dotfiles_dir/vim ~/.vim
+ln -sf $dotfiles_dir/vimrc/basic.vim ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
 
-ln -fs `pwd`/bash_profile  ~/.bash_profile
-ln -fs `pwd`/bash_aliases  ~/.bash_aliases
-echo "Update bash_profile"
+ln -sf $dotfiles_dir/bashrc ~/.bashrc
+ln -sf $dotfiles_dir/bash_profile  ~/.bash_profile
+ln -sf $dotfiles_dir/bash_aliases  ~/.bash_aliases
+ln -sf $dotfiles_dir/bash_  ~/.bash_aliases
+echo "Update bash_profile, Copy bash_local if need"
 
-ln -fs `pwd`/tmux.conf  ~/.tmux.conf
-echo "Update tmux"
+#ln -sf $dotfiles_dir/linux-tmux ~/.tmux
+#ln -sf $dotfiles_dir/zsh/zsh_prompt ~/.zsh_prompt
+#ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
+ln -sf $dotfiles_dir/config ~/.config
+ln -sf $dotfiles_dir/custom-configs/custom-snips ~/.vim/custom-snips
+
+#Load Tmux plugins
+#git clone --quiet https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#ln -fs `pwd`/tmux.conf  ~/.tmux.conf
+#echo "Update tmux"
+
+#==============
+# Give the user a summary of what has been installed
+#==============
+echo -e "\n====== Summary ======\n"
+cat $log_file
+echo
+echo "Enjoy!"
+rm $log_file
+
